8555b0681b0c06dadf67796fd306ab582136aaee
DaniilPankow <pankov.daniil@chnu.edu.ua>
2024-11-04 21:48:01 +0200

    Добавьте файлы проекта.

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class CurrencyConverter
+{
+    public static double ConvertCurrency(double amount, double exchangeRate)
+    {
+        return amount * exchangeRate;
+    }
+}
+
